1e511c9eca2cdba5d0a718e9fe7dc871ead2a5cc
Nikola Vukojevic <117970622+nikolavukojevic-nv@users.noreply.github.com>
2025-07-23 12:22:13 +0200

    local to github
    second computational try

---
+++ strength calculator.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import time
+import csv
+
+# 1.0 input
+datetime = time.localtime()
+set = input('describe the set: ')
+energy = float(input('energy: [kJ] '))
+price = float(input('price: [€] '))
+
+time = float(input('lasting duration (time): [s] '))
+
+# 2.0 tranformation
+
+'''
+x1 ... observable
+m ... mass
+t ... time
+d ... distance
+v = d/t
+a = v/t
+F = m*a
+W = F * d
+P = W / t
+E = P * mf1
+mf ... modifying factor 1
+R = P / t
+St = R / price
+
+'''
+power = energy/3.6
+robustness = power/time
+energy_price = energy/price
+power_price = power/price
+strength = robustness/price
+
+'''
+print('-------------------\n')
+print(datetime)
+print('-',set)
+print('Energy per Money: ',energy_price, 'kJ/€')
+print('Power per Money: ',power_price, 'W/s€')
+print('Strength: ',strength, 'W/s^2€')
+
+the_strong = 'wheat flour'
+'''
+
+
+# 2.1 updating
+
+data_entry = (datetime,set,strength)
+print(data_entry)
+
+# 2.1.1 writing results to a .csv document
+
+
+# 2.2 storing sets into a pandas dataframe and sorting descending
+
+strength_data = pd.read_csv('strength data.csv')
+
+
+# 2.3 storing the updated strongest set in the the_strong variable
+
+print(strength_data.head(1))
+the_strong = strength_data.head(1)
+
+# 2.4 normalizing and visualizing
+
+# 2.5 normalizing
+
+# 2.6 visualizing
+
+
+# 3.0 output
+print('\n---------\n>',the_strong)
