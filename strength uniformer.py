e6f2163ea0331895ba77905c4b8db497b7cc1f0d
Nikola Vukojevic <117970622+nikolavukojevic-nv@users.noreply.github.com>
2025-10-26 09:21:51 +0100

    added money calculator
    based on uniform strength, you’ll enter your robustness and get the corresponding money.

---
+++ strength uniformer.py
@@ -0,0 +1,8 @@
+# strength uniformer
+
+R_i = float(input('enter robustness: [W/s^2]'))
+St_uni = 1.0
+M_i = R_i / St_uni
+print('------------\nyou have',M_i,'€')
+
+'Based on the given robustness R_i give the corresponding money M_i to it.'
